80699b5c4d034d157e83de550c690d54dc24c89e
Pierre Matricon <pierre.matricon@gmail.com>
2020-02-17 11:46:07 +0100

    Create install_q.sh

---
+++ 5_prep_topologies/install_q.sh
@@ -0,0 +1,12 @@
+mkdir qsource_test_13018
+cd qsource_test_13018
+git clone https://github.com/qusers/qsource.git --branch testing/akesandgren --single-branch qakesandgren
+cd Q5
+git fetch
+git checkout bugfix
+git pull
+cd src
+module load intel/17.0.4.196
+module load impi/5.1.3
+make mpi COMP=ifort
+make all COMP=ifort
